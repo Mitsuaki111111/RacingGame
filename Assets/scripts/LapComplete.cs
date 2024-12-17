b338a5920fa07d732ca7bca273c306141e53adeb
Mitsuaki_7 <tky2302120@stu.o-hara.ac.jp>
2024-12-17 16:42:09 +0900

    ベストラップ'(仮)*要改善

--- Assets/scripts/LapComplete.cs
+++ Assets/scripts/LapComplete.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LapComplete : MonoBehaviour
 {
@@ -8,4 +10,35 @@ public class LapComplete : MonoBehaviour
     public GameObject HalfLapTrig;
 
     public GameObject MinuteDisplay;
+    public GameObject SecondDisplay;
+    public GameObject MilliDisplay;
+
+    public GameObject LapTimeBox;
+
+    private void OnTriggerEnter()
+    {
+        if (LapTimeManager.SecondCount <= 9)
+        {
+            SecondDisplay.GetComponent<TextMeshProUGUI>().text = "0" + LapTimeManager.SecondCount + ".";
+        }
+        else
+        {
+            SecondDisplay.GetComponent<TextMeshProUGUI>().text = "" + LapTimeManager.SecondCount + ".";
+        }
+        if(LapTimeManager.MinuteCount <= 9) 
+        {
+            MinuteDisplay.GetComponent<TextMeshProUGUI>().text = "0" + LapTimeManager.MinuteCount + ".";
+            
+        }
+        else
+        {
+            MinuteDisplay.GetComponent<TextMeshProUGUI>().text = "" + LapTimeManager.MinuteCount + ".";
+        }
+
+        MilliDisplay.GetComponent<TextMeshProUGUI>().text = "" + LapTimeManager.MilliCount;
+
+        LapTimeManager.MinuteCount = 0;
+        LapTimeManager.SecondCount = 0;
+        LapTimeManager.MilliCount = 0;
+    }
 }
